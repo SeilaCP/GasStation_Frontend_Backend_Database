e9b4b797ee39cbda5d8373b4d6ba34be4d1c7222
manhseila@gmail.com <manhseila@gmail.com>
2025-06-19 18:13:42 +0700

    backup front end

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const customerRouter = require('./routes/CustomerRoutes');  
 const port = 3000;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/customers', customerRouter);
