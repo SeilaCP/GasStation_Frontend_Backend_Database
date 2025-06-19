b8ff844ae543e62bc4c434ce8d30ae907c0742d5
manhseila@gmail.com <manhseila@gmail.com>
2025-06-19 18:53:36 +0700

    hide password

--- backend/src/utils/database.js
+++ backend/src/utils/database.js
@@ -4,7 +4,7 @@ const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'dvdrental',
-  password: 'Alg0r1thm@c#',
+  password: 'YOUR_PASSWORD', // replace with your actual password
   port: 5432,
 });
 
